8c901df6859ebee9be66c3ef15d6ac3acb3c132c
Aaron Miles <cool_jingle@hotmail.co.uk>
2017-09-20 16:51:55 +0100

    further script fixes

--- Memrise_Reverse_Translations.user.js
+++ Memrise_Reverse_Translations.user.js
@@ -4,7 +4,7 @@
 // @description    Reverse testing direction when using Memrise
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
-// @version        0.0.5
+// @version        0.0.6
 // @updateURL      https://github.com/cooljingle/memrise-reverse-translations/raw/master/Memrise_Reverse_Translations.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-reverse-translations/raw/master/Memrise_Reverse_Translations.user.js
 // @grant          none
@@ -27,10 +27,50 @@ $(document).ready(function() {
         var courseId,
             element = $('#reverse-translations'),
             isReversed;
+
+        MEMRISE.garden.box_mapping.multiple_choice = MEMRISE.garden.box_mapping.reversed_multiple_choice = (function(){
+            var mc = MEMRISE.garden.box_types.MultipleChoiceBox;
+            var rmc = MEMRISE.garden.box_types.ReversedMultipleChoiceBox;
+            return function() {
+                if(isReversed){
+                    arguments[0].template = "reversed_multiple_choice";
+                    return new rmc(...arguments);
+                } else {
+                    arguments[0].template = "multiple_choice";
+                    return new mc(...arguments);
+                }
+            };
+        }());
+
         element.click(function() {
             isReversed = !isReversed;
             setReversedState(true);
         });
+
+        MEMRISE.garden.session.box_factory.make = (function () {
+            var cached_function = MEMRISE.garden.session.box_factory.make;
+            return function () {
+                courseId = MEMRISE.garden.session.course_id || MEMRISE.garden.session_data.things_to_courses[this.thinguser.thing_id];
+                isReversed = reversedCourses.indexOf(courseId) > -1;
+                setReversedState();
+                return cached_function.apply(this, arguments);
+            };
+        }());
+
+        MEMRISE.garden.box_types.TypingTestBox.prototype.activate = (function () {
+            var cached_function = MEMRISE.garden.box_types.TypingTestBox.prototype.activate;
+            return function () {
+                if(isReversed) {
+                    [this.learnable.item, this.learnable.definition] = [this.learnable.definition, this.learnable.item];
+                    [this.correct, this.prompt] = [this.prompt, this.correct];
+                    this.accepted = [this.correct];
+                }
+                var result = cached_function.apply(this, arguments);
+                if(isReversed)
+                    [this.learnable.item, this.learnable.definition] = [this.learnable.definition, this.learnable.item];
+                return result;
+            };
+        }());
 
         function setReversedState(setStorage) {
             if(isReversed) {
@@ -49,29 +89,5 @@ $(document).ready(function() {
                     .attr("title", "Reverse testing direction for this course");
             }
         }
-
-        _.each(MEMRISE.garden.box_types, function (box_type) {
-            box_type.prototype.activate = (function () {
-                var cached_function = box_type.prototype.activate;
-                return function () {
-                    courseId = MEMRISE.garden.session.course_id || MEMRISE.garden.session_data.things_to_courses[this.thinguser.thing_id];
-                    isReversed = reversedCourses.indexOf(courseId) > -1;
-                    var didReverse = false;
-                    if(this instanceof MEMRISE.garden.box_types.TestBox) {
-                        if(isReversed && this.promptWith === "text" && this.testKind === "text") {
-                            didReverse = true;
-                            [this.learnable.item, this.learnable.definition] = [this.learnable.definition, this.learnable.item];
-                            [this.correct, this.prompt] = [this.prompt, this.correct];
-                            _.filter(this.choices, c => c.correct).forEach(c => c.choice = c.choice_html = this.correct);
-                        }
-                    }
-                    var result = cached_function.apply(this, arguments);
-                    if(didReverse)
-                        [this.learnable.item, this.learnable.definition] = [this.learnable.definition, this.learnable.item];
-                    setReversedState();
-                    return result;
-                };
-            }());
-        });
     }
 });
